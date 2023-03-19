cd12a8efdd27271ab36ba615717e73600433bea3
olahurshy <olahurshy@yahoo.co.za>
2023-03-19 16:38:44 -0400

    upgrade bootstrap and jquery versions

--- admin/chat.php
+++ admin/chat.php
@@ -3,7 +3,8 @@ session_start();
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=noob;charset=utf8', 'root', '');
 $reponse = $bdd->query('SELECT * FROM chat ORDER BY id');
 ?>
-<link href="bootstrap.css" rel="stylesheet" id="bootstrap-css">
+<!-- <link href="bootstrap.css" rel="stylesheet" id="bootstrap-css"> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap-grid.min.css" integrity="sha512-Aa+z1qgIG+Hv4H2W3EMl3btnnwTQRA47ZiSecYSkWavHUkBF2aPOIIvlvjLCsjapW1IfsGrEO3FU693ReouVTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js" integrity="sha512-EKWWs1ZcA2ZY9lbLISPz8aGR2+L7JVYqBAYTq5AXgBkSjRSuQEGqWx8R1zAX16KdXPaCjOCaKE8MCpU0wcHlHA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!------ Include the above in your HEAD tag ---------->
